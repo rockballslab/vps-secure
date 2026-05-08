c6a230dd4be72d8c695e26bfca741cc72dd4c254
rockballslab <server2fab@gmail.com>
2026-05-08 09:08:42 +0200

    Add files via upload

--- dashboard/api/app.py
+++ dashboard/api/app.py
@@ -1541,7 +1541,7 @@ def get_containers() -> list:
         s = stats_map.get(name, {})
 
         result.append({
-            'name':        name,
+            'name': name.replace('saaskit-', '', 1),   # strip prefix cosmétique pour le display
             'image':       image,
             'image_clean': image_clean,
             'state':       state,
